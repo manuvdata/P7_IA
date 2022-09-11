148e77313882771542044572b2a6b657de578a5d
ManuVaillant <evaillant.data@gmail.com>
2022-09-11 20:41:46 +0200

    final?

--- Prediction/apps.py
+++ Prediction/apps.py
@@ -10,12 +10,12 @@ class PredictionConfig(AppConfig):
     name = 'Prediction'
     BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
     CLASSIFIER_FOLDER = os.path.join(BASE_DIR, 'Prediction/bert/')
-    url = 'https://drive.google.com/uc?id=1211JqjGW5zYkJMO1bVjsrsS0Xd6mmX_V'
+    url = 'https://drive.google.com/uc?id=11LofCQX-DXYQbAYYUXCy5YGJ7lBgsmJn'
     output = 'manugdown.h5'
     #model_save_path =gdown.download(url,output)
  
     num_classes=2
-    bert_tokenizer = BertTokenizer.from_pretrained("bert-large-uncased")
+    bert_tokenizer = BertTokenizer.from_pretrained("bert-base-cased")
     #model_save_path=os.path.join(CLASSIFIER_FOLDER, "bert_model.h5")
     trained_model = TFBertForSequenceClassification.from_pretrained('bert-base-cased',num_labels=2)
     trained_model.load_weights(gdown.download(url,output))
